823c53b1221b44d76bd881a181f93d6593eadf1a
Layo Kaminski <layokaminski@gmail.com>
2021-02-28 17:13:08 -0300

    Repassando states do componente countdown para o contexto countdown

--- moveit-next/src/contexts/Countdown.tsx
+++ moveit-next/src/contexts/Countdown.tsx
@@ -1,11 +1,29 @@
-import { createContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
+import { ChallengesContext } from "./ChallengesContext";
 
 interface CountdownContextData {
 
 }
 
+interface CountdownProviderProps {
+  children: ReactNode;
+}
+
 const CountdownContext = createContext({} as CountdownContextData);
 
-export function CountdownProvider() {
-  
+export function CountdownProvider({ children }: CountdownProviderProps) {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [ time, setTime ] = useState(0.1 * 60);
+  const [ isActive, setIsActive ] = useState(false);
+  const [ hasFinished, setHasFinisehd ] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  return (
+    <CountdownContext.Provider value={{}}>
+      { children }
+    </CountdownContext.Provider>
+  )
 }
