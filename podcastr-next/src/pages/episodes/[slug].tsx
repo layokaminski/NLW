0e799f053c7c6751aeb2c87f0cc4a5f85fc84ca1
Layo Kaminski <layokaminski@gmail.com>
2021-04-22 17:09:51 -0300

    Finalizado a estilização da page episodes

--- podcastr-next/src/pages/episodes/[slug].tsx
+++ podcastr-next/src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { format, parseISO } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 import styles from './episode.module.scss';
@@ -26,9 +27,11 @@ const Episode = ({ episode }: EpisodeProps) => {
   return (
     <div className={ styles.episode }>
       <div className={ styles.thumbnailContainer }>
-        <button type="button">
-          <img src="/arrow-left.svg" alt="Voltar"/>
-        </button>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar"/>
+          </button>
+        </Link>
         <Image
           width={ 700 }
           height={ 160 }
