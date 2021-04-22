f647e36a7045358ae193ed9c770e0c5e73225165
Layo Kaminski <layokaminski@gmail.com>
2021-04-22 11:47:20 -0300

    Gerando Home de forma estática

--- podcastr-next/src/pages/index.tsx
+++ podcastr-next/src/pages/index.tsx
@@ -3,3 +3,16 @@ export default function Home() {
     <div>Index</div>
   );
 }
+
+export const getStaticProps = async () => {
+  const url = 'http://localhost:3333/episodes';
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
