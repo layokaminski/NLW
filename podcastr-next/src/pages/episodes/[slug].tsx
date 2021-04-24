d726a76d1234ab0b5a6d80db3d48a88ea1ed11f8
Layo Kaminski <layokaminski@gmail.com>
2021-04-24 04:52:16 -0300

    Ajuste na barra do navegador para estilização da página

--- podcastr-next/src/pages/episodes/[slug].tsx
+++ podcastr-next/src/pages/episodes/[slug].tsx
@@ -1,6 +1,7 @@
 import { format, parseISO } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePlayer } from '../../contexts/PlayerContext';
@@ -29,6 +30,9 @@ const Episode = ({ episode }: EpisodeProps) => {
 
   return (
     <div className={ styles.episode }>
+      <Head>
+        <title>{ episode.title } | Podcastr</title>
+      </Head>
       <div className={ styles.thumbnailContainer }>
         <Link href="/">
           <button type="button">
